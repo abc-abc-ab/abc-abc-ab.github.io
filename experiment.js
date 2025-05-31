4915b5f09ac2111e285bc87d24a6394cf7ae558d
Marui <152158429+abc-abc-ab@users.noreply.github.com>
2025-05-31 17:00:33 +0900

    Commit Changes

--- experiment.js
+++ experiment.js
@@ -1,12 +1,17 @@
+
+/** @type {IDBDatabase} `IDBDatabase`*/
 let db;
 // Open the database
 const request = window.indexedDB.open("Time", 1);
 
+// Error handling
 request.onerror = (event) => {
-  console.error(
+  console.log("Error opening database: %s", request.error.message);
+  alert(
     "Sorry, you can't use IndexedDB.",
   );
 };
-request.onsuccess = (event) => {
-  db = event.target.result;
+// Successful processing
+request.onsuccess = /** @this {IDBRequest<IDBDatabase>} `IDBRequest<IDBDatabase>` @param {Event} event */(event) => {
+  console.log("Database opened successfully");
 };
