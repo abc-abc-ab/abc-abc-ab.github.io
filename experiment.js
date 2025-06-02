54e7f6c46da80daabc108741961fd80dce15d42c
Marui <152158429+abc-abc-ab@users.noreply.github.com>
2025-06-02 09:35:12 +0900

    Commit Changes

--- experiment.js
+++ experiment.js
@@ -1,5 +1,5 @@
 "use strict";
-// JavaScript2.4.0
+// JavaScript2.4.1
 const DB_NAME = "Experiments", // Database name
 DB_VERSION = 2, // Database version ver.2
 DB_STORE_NAME = "Times", // Object store name
